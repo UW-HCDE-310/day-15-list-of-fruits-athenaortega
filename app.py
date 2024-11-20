0cd1adcbd620f4819ab6346fe0d3920dbe92635d
athenaortega <160282703+athenaortega@users.noreply.github.com>
2024-11-20 13:16:55 -0800

    Completed exercises to loop through fruits and display the # of fruits while also displaying any fruits that start with o. Also created API Keys.

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
+import keys
+
 app = Flask(__name__)
 @app.route("/")
 def index():
     fruits = [
-        {"name": "apples", "quantity": 3},
+        #{"name": "apples", "quantity": 3},
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
-    return render_template("index.html", fruits=fruits)
+    return render_template("index.html", fruits=fruits, key_1=keys.API_KEY, key_2=keys.API_KEY_2)
